69920e6b43e220b3a549f43f69cd2e03a38e6bb4
DimanT <tyukilin555@gmail.com>
2025-08-25 21:14:44 +0400

    refactor: добавил аннотации get/set

--- src/main/java/org/pet/dto/ExchangeRateResponseDTO.java
+++ src/main/java/org/pet/dto/ExchangeRateResponseDTO.java
@@ -1,44 +1,19 @@
 package org.pet.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.pet.entity.Currency;
 
 import java.math.BigDecimal;
 
-public class ExchangeRateResponseDTO extends BaseDto {
+@Builder
+@Getter
+@Setter
+
+public class ExchangeRateResponseDTO {
     private int id;
     private Currency baseCurrency;
     private Currency targetCurrency;
     private BigDecimal rate;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Currency getBaseCurrency() {
-        return baseCurrency;
-    }
-
-    public void setBaseCurrency(Currency baseCurrency) {
-        this.baseCurrency = baseCurrency;
-    }
-
-    public Currency getTargetCurrency() {
-        return targetCurrency;
-    }
-
-    public void setTargetCurrency(Currency targetCurrency) {
-        this.targetCurrency = targetCurrency;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
 }
