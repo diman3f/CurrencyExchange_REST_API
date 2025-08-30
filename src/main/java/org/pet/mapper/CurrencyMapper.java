44d3044f565cb4299e322d0fddcd66e5a6c5535b
DimanT <tyukilin555@gmail.com>
2025-08-30 07:31:34 +0400

    chore: метод toCurrencyRequestDTO

--- src/main/java/org/pet/mapper/CurrencyMapper.java
+++ src/main/java/org/pet/mapper/CurrencyMapper.java
@@ -1,15 +1,23 @@
 package org.pet.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.pet.dto.CurrencyDTO;
+import org.pet.dto.CurrencyRequestDto;
 import org.pet.entity.Currency;
 
 import java.util.List;
+import java.util.Set;
 
 @Mapper
 public interface CurrencyMapper {
     CurrencyMapper INSTANCE = Mappers.getMapper(CurrencyMapper.class);
+
+    @Mapping(target = "name", source = "nameCurrency")
+    @Mapping(target = "code", source = "codeCurrency")
+    @Mapping(target = "sign", source = "signCurrency")
+    CurrencyRequestDto toCurrencyRequestDTO(String nameCurrency, String codeCurrency, String signCurrency);
     CurrencyDTO toCurrencyDTO(Currency currency);
     List<CurrencyDTO> toCurrencyDTOList(List<Currency> currencies);
     Currency toCurrency(CurrencyDTO currencyDTO);
