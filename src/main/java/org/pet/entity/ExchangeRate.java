617f8801a8585abc1f10662b2d25e99ff99fdef7
DimanT <tyukilin555@gmail.com>
2025-08-30 07:48:01 +0400

    refactor: добавил lombok аннотации

--- src/main/java/org/pet/entity/ExchangeRate.java
+++ src/main/java/org/pet/entity/ExchangeRate.java
@@ -1,48 +1,17 @@
 package org.pet.entity;
 
 import lombok.Builder;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.math.BigDecimal;
 
 @Builder
+@Getter
+@Setter
 public class ExchangeRate {
     private int id;
     private int baseCurrencyId;
     private int targetCurrencyId;
     private BigDecimal rate;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getBaseCurrencyId() {
-        return baseCurrencyId;
-    }
-
-    public void setBaseCurrencyId(int baseCurrencyId) {
-        this.baseCurrencyId = baseCurrencyId;
-    }
-
-    public int getTargetCurrencyId() {
-        return targetCurrencyId;
-    }
-
-    public void setTargetCurrencyId(int targetCurrencyId) {
-        this.targetCurrencyId = targetCurrencyId;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
-
-
 }
 
