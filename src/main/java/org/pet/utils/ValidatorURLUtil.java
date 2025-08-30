aa564827692d40764bd46a73ec01032af67f72b7
DimanT <tyukilin555@gmail.com>
2025-08-30 08:51:43 +0400

    refactor: добавил вспомогательные методы для валидации

--- src/main/java/org/pet/utils/ValidatorURLUtil.java
+++ src/main/java/org/pet/utils/ValidatorURLUtil.java
@@ -1,28 +1,20 @@
 package org.pet.utils;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.pet.exception.ValidationException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ValidatorURLUtil {
-    public static boolean hasValidCodeCurrencyFormat(HttpServletRequest req) {
-        String pathInfo = req.getPathInfo();
-        String[] pathParts = pathInfo.split("/");
-        return pathParts.length == 2;
-    }
 
-    public static boolean hasValidCurrencyPairFormat(HttpServletRequest req) {
-        String pathInfo = req.getPathInfo();
-        String[] pathParts = pathInfo.split("/");
-        return pathParts.length == 5;
+    private ValidatorURLUtil() {
+        throw new ArrayStoreException("Нельзя создать объект статического класса");
     }
 
     public static List<String> getValidPairCurrencyPairFormat(String pathUrl) {
+
         String[] pathParts = pathUrl.split("/");
-        if (pathUrl.length() > 1) {
-            String currencyPair = pathParts[1];
+        if (hasMultipleFragments(pathUrl)) {
+            String currencyPair = getPairCurrencyOfURLPath(pathParts);
             isValidLengthPairCurrencyCode(currencyPair);
             String baseCode = currencyPair.substring(0, 3);
             String targetCode = currencyPair.substring(3);
@@ -32,9 +24,15 @@ public class ValidatorURLUtil {
         throw new ValidationException("Длина валютной пары должна быть 6 символов из латинских заглавных букв");
     }
 
+    private static boolean hasMultipleFragments(String pathUrl) {
+        return pathUrl.length() > 1;
+    }
+
+    private static String getPairCurrencyOfURLPath(String[] pathParts) {
+        return pathParts[1];
+    }
 
     private static void isValidLengthPairCurrencyCode(String currencyPair) {
-
         if (currencyPair == null || !currencyPair.matches("[A-Z]{6}")) {
             throw new ValidationException("Длина валютной пары должна быть 6 символов из латинских заглавных букв");
         }
