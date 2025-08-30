13a9a55013ec34bb53122e857afddde1504fcd62
DimanT <tyukilin555@gmail.com>
2025-08-30 08:21:05 +0400

    refactor: дабавил обработчик ошибок

--- src/main/java/org/pet/servlets/ExchangeServlet.java
+++ src/main/java/org/pet/servlets/ExchangeServlet.java
@@ -2,19 +2,21 @@ package org.pet.servlets;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.pet.HttpStatus;
 import org.pet.dto.CurrencyExchangeRateRequestDto;
 import org.pet.dto.CurrencyExchangeRateResponseDto;
-import org.pet.exception.ExchangeRateException;
 import org.pet.mapper.CurrencyExchangeMapper;
 import org.pet.services.ExchangeRateService;
+import org.pet.utils.ExceptionHandlerUtil;
 import org.pet.utils.JsonResponseBuilder;
 
 import java.io.IOException;
 
 @WebServlet("/exchange/*")
-public class ExchangeServlet extends ExceptionHandler {
+public class ExchangeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -26,7 +28,7 @@ public class ExchangeServlet extends ExceptionHandler {
             CurrencyExchangeRateResponseDto currencyExchangeRateResponseDto = ExchangeRateService.getINSTANCE().executeExchangeCurrency(dto);
             JsonResponseBuilder.buildJsonResponse(resp, currencyExchangeRateResponseDto, HttpStatus.OK);
         } catch (RuntimeException e) {
-            throw new ExchangeRateException(e.getMessage());
+            ExceptionHandlerUtil.handleException(resp,e);
         }
     }
 }
