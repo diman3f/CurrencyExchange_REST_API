d33b8bdd5be7775be6bbe77ce774071f717481c6
DimanT <tyukilin555@gmail.com>
2025-08-30 08:53:25 +0400

    chore: добавил исключение по ошибки соединения с бд

---
+++ src/main/java/org/pet/exception/DataBaseConnectException.java
@@ -0,0 +1,7 @@
+package org.pet.exception;
+
+public class DataBaseConnectException extends DataBaseException{
+    public DataBaseConnectException(String message) {
+        super(message);
+    }
+}
